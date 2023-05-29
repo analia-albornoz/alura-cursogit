c853f829d855264ab9da6be0e7981e4a5f9b50ff
Analia <nanito1112@gmail.com>
2023-05-29 20:27:24 -0300

    Realizando el encripatdor/desencriptador

---
+++ encriptar.js
@@ -0,0 +1,8 @@
+function encriptacion(texto) {
+    var encriptado = texto.replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/a/gi, 'ai').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');   
+    return encriptado;
+}  
+function desencriptacion(texto){
+    var desencriptado = texto.replace(/ai/gi, "a").replace(/enter/gi, "e").replace(/imes/gi, "i").replace(/ober/gi, "o").replace(/ufat/gi, "u");
+    return desencriptado;
+}
